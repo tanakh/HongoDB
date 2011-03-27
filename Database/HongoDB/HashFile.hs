e88884d90f7120386eaef3c286feb28fc9a0143d
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2011-03-28 04:46:32 +0900

    implement HashFile

--- Database/HongoDB/HashFile.hs
+++ Database/HongoDB/HashFile.hs
@@ -1,27 +1,392 @@
+{-# Language OverloadedStrings, GeneralizedNewtypeDeriving, TupleSections #-}
+
 module Database.HongoDB.HashFile (
+  HashFile,
+  openHashFile, closeHashFile,
+  runHashFile,
   ) where
 
-import Database.HongoDB.Base
+import qualified Database.HongoDB.Base as H
 import qualified Database.HongoDB.Internal.File as F
 
+import qualified Blaze.ByteString.Builder as BB
+import Control.Applicative
+import Control.Concurrent.MVar
+import qualified Control.Exception.Control as MC
+import Control.Monad.IO.Control
+import Control.Monad.Trans
+import Control.Monad.Reader
+import qualified Data.Attoparsec as A
+import qualified Data.Attoparsec.Binary as A
+import Data.Bits
+import qualified Data.ByteString as B
+import Data.Hashable
+import Data.Int
+import Data.IORef
+import Data.Monoid
+import System.Directory
+
+import Prelude hiding (lookup)
+
+magicString = "HHDB" :: B.ByteString
+formatVersion = (0 :: Int8, 0 :: Int8)
+
 newtype HashFile m a =
-  HashFile { unHashFile :: ReaderT File m a }
-  deriving (Monad, MonadIO, MonadTrans, Functor, Applicative)
+  HashFile { unHashFile :: ReaderT HashFileState m a }
+  deriving (Monad, MonadIO, MonadTrans, Functor, Applicative, MonadControlIO)
 
-openHashFile :: FilePath -> IO HashFile
+data HashFileState =
+  HashFileState
+  { file   :: F.File
+  , header :: IORef Header
+  , lock :: MVar ()
+  }
+
+openHashFile :: FilePath -> IO F.File
 openHashFile path = do
-  b <- doesFileExists path
-  if b
-    then do
-    f <- F.open path
+  b <- doesFileExist path
+  f <- F.open path
+  when (not b) $ do
     initHashFile f
-    return f
+  return f
+
+closeHashFile :: F.File -> IO ()
+closeHashFile f =
+  F.close f
+
+data Header =
+  Header
+  { magic          :: B.ByteString
+  , version        :: (Int8, Int8)
+  , bucketSize     :: Int
+  , recordSize     :: Int
+  , freeBlockSize  :: Int
+  , fileSize       :: Int
+  , bucketStart    :: Int
+  , freeBlockStart :: Int
+  , recordStart    :: Int
+  }
+  deriving (Show)
+
+fromHeader :: Header -> BB.Builder
+fromHeader h = BB.fromWrite $
+  BB.writeByteString (magic h) `mappend`
+  BB.writeInt8 (fst $ version h) `mappend`
+  BB.writeInt8 (snd $ version h) `mappend`
+  writeInt48le (bucketSize h) `mappend`
+  writeInt48le (recordSize h) `mappend`
+  writeInt48le (freeBlockSize h) `mappend`
+  writeInt48le (fileSize h) `mappend`
+  writeInt48le (bucketStart h) `mappend`
+  writeInt48le (freeBlockStart h) `mappend`
+  writeInt48le (recordStart h)
+
+writeInt48le :: Int -> BB.Write
+writeInt48le n =
+  BB.writeInt32le (fromIntegral n) `mappend`
+  BB.writeInt16le (fromIntegral $ n `shiftR` 32)
+
+writeVInt :: Int -> BB.Write
+writeVInt 0 = BB.writeWord8 0
+writeVInt n
+  | n < 128 =
+    BB.writeWord8 (fromIntegral n)
+  | otherwise =
+    BB.writeWord8 (fromIntegral $ n `mod` 128) `mappend`
+    writeVInt (n `div` 128)
+
+-- TODO: optimize
+vlen :: Int -> Int
+vlen = B.length . BB.toByteString . BB.fromWrite . writeVInt
+
+parseHeader :: A.Parser Header
+parseHeader =
+  Header
+  <$> A.take 4
+  <*> ((,)
+       <$> (fromIntegral <$> A.anyWord8)
+       <*> (fromIntegral <$> A.anyWord8))
+  <*> anyWord48le
+  <*> anyWord48le
+  <*> anyWord48le
+  <*> anyWord48le
+  <*> anyWord48le
+  <*> anyWord48le
+  <*> anyWord48le
+
+anyWord48le :: A.Parser Int
+anyWord48le = do
+  a <- fromIntegral <$> A.anyWord32le
+  b <- fromIntegral <$> A.anyWord16le
+  return $ a .|. (b `shiftL` 32)
+
+anyVInt :: A.Parser Int
+anyVInt = do
+  n <- A.anyWord8
+  if n < 128
+    then return (fromIntegral n)
     else do
-    F.open path
+    r <- anyVInt
+    return $ (fromIntegral n .&. 0x7f) + r * 128
 
-initHashFile :: File -> IO ()
-initHashFIle f = do
-  
+toHeader :: B.ByteString -> Header
+toHeader bs =
+  case A.parse parseHeader bs of
+    A.Done _ r ->
+      r
+    _ ->
+      error "toHeader: no header"
 
---instance DB (HashFile m) where
---  accept :: 
+toInt48le :: B.ByteString -> Int
+toInt48le bs =
+  case A.parse anyWord48le bs of
+    A.Done _ r ->
+      r
+    _ ->
+      error "toInt48le: fail"
+
+nextPrime :: Int -> Int
+nextPrime n = head $ filter isPrime [n..] where
+  isPrime n = and [ n`mod`i /= 0
+                  | i <- [2 .. floor (sqrt (fromIntegral n))]]
+
+emptyHeader :: Header
+emptyHeader =
+  Header
+  { magic = magicString
+  , version = formatVersion
+  , bucketSize = 0
+  , recordSize = 0
+  , freeBlockSize = 0
+  , fileSize = 0
+  , bucketStart = 0
+  , freeBlockStart = 0
+  , recordStart = 0
+  }
+
+initialHeader :: Header
+initialHeader =
+  emptyHeader
+  { bucketSize = bsize
+  , freeBlockSize = fbsize
+  , fileSize = fsize
+  , bucketStart = bstart
+  , freeBlockStart = fstart
+  , recordStart = rstart
+  }
+  where
+    bsize = nextPrime 1024
+    fbsize = 64
+    bstart = headerSize
+    fstart = bstart + bsize * 6
+    rstart = fstart + fbsize * 6
+    fsize = rstart
+
+headerSize :: Int
+headerSize =
+  B.length $ BB.toByteString $ fromHeader emptyHeader
+
+initHashFile :: F.File -> IO ()
+initHashFile f = do
+  let h = initialHeader
+  F.write f (BB.toByteString $ fromHeader h) 0
+  F.write f (B.replicate (bucketSize h * 6) 0xff) (bucketStart h)
+  F.write f (B.replicate (freeBlockSize h * 6) 0xff) (freeBlockStart h)
+
+emptyEntry :: Int
+emptyEntry = 0xffffffffffff
+
+readHeader :: F.File -> IO Header
+readHeader f = do
+  toHeader <$> F.read f headerSize 0
+
+runHashFile :: MonadControlIO m => F.File -> HashFile m a -> m a
+runHashFile f db = do
+  h <- liftIO $ readHeader f
+  r <- liftIO $ HashFileState f <$> newIORef h <*> newMVar ()
+  runReaderT (unHashFile db) r
+
+--
+
+data Record =
+  Record
+  { rnext :: Int
+  , rkey  :: B.ByteString
+  , rval  :: B.ByteString
+  }
+  deriving (Show)
+
+fromRecord :: Record -> B.ByteString
+fromRecord r = BB.toByteString $ BB.fromWrite $
+  writeInt48le (rnext r) `mappend`
+  writeVInt (B.length $ rkey r) `mappend`
+  writeVInt (B.length $ rval r) `mappend`
+  BB.writeByteString (rkey r) `mappend`
+  BB.writeByteString (rval r)
+
+-- TODO: optimize
+parseRecord :: A.Parser Record
+parseRecord = do
+  rn <- anyWord48le
+  klen <- anyVInt
+  vlen <- anyVInt
+  Record rn <$> A.take klen <*> A.take vlen
+
+parseRecordHeader :: A.Parser Record
+parseRecordHeader = do
+  rn <- anyWord48le
+  klen <- anyVInt
+  vlen <- anyVInt
+  return $ Record rn (B.replicate klen 0) (B.replicate vlen 0)
+
+readPartialRecord :: Int -> HashFileState -> IO (Record, Bool)
+readPartialRecord ofs HashFileState { file = f, header = hr } = do
+  h <- readIORef hr
+  bs <- F.read f 64 (recordStart h + ofs)
+  case A.parse parseRecord bs of
+    A.Done _ r -> return (r, True)
+    A.Partial _ -> case A.parse parseRecordHeader bs of
+      A.Done _ r -> return (r, False)
+      _ -> error "readPartial: failed"
+    _ -> error "readPartial: failed"
+
+readCompleteRecord :: Int -> Record -> HashFileState -> IO Record
+readCompleteRecord ofs r HashFileState { file = f, header = hr } = do
+  let rsize = 6 +
+              vlen (B.length $ rkey r) + vlen (B.length $ rval r) +
+              B.length (rkey r) + B.length (rval r)
+  h <- readIORef hr
+  bs <- F.read f rsize (recordStart h + ofs)
+  case A.parse parseRecord bs of
+    A.Done _ r -> return r
+    _ -> error "readComplete: failed"
+
+addRecord :: Record -> HashFileState -> IO Int
+addRecord r stat @ (HashFileState { file = f, header = hr }) = do
+  -- TODO: first search free pool
+  h <- readIORef hr
+  let st = recordStart h
+      end = fileSize h
+  nend <- writeRecord (end - st) r stat
+  writeIORef hr $ h { fileSize = st + nend }
+  return (end - st)
+
+writeRecord :: Int -> Record -> HashFileState -> IO Int
+writeRecord ofs r stat = do
+  h <- readIORef (header stat)
+  let bs = fromRecord r
+  F.write (file stat) bs (recordStart h + ofs)
+  return $ ofs + B.length bs
+
+-- TODO: optimize
+sizeRecord :: Record -> Int
+sizeRecord = B.length . fromRecord
+
+writeNext :: Int -> Int -> HashFileState -> IO ()
+writeNext ofs next stat = do
+  st <- fileSize <$> readIORef (header stat)
+  F.write (file stat) (BB.toByteString $ BB.fromWrite $ writeInt48le next) (st + ofs)
+
+readBucket :: Int -> HashFileState -> IO Int
+readBucket bix stat = do
+  bofs <- ((+ bix * 6) . bucketStart <$> readIORef (header stat))
+  bs <- F.read (file stat) 6 bofs
+  return $ toInt48le bs
+
+writeBucket :: Int -> Int -> HashFileState -> IO ()
+writeBucket bix val stat =
+  F.write
+  (file stat)
+  (BB.toByteString $ BB.fromWrite $ writeInt48le val)
+  =<< ((+ bix * 6) . bucketStart <$> readIORef (header stat))
+
+lookup :: B.ByteString -> HashFileState -> IO (Maybe B.ByteString)
+lookup key stat = do
+  mb <- lookup' key stat
+  return $ rval . fst <$> mb
+
+lookup' :: B.ByteString -> HashFileState ->
+           IO (Maybe (Record, (Int, Int)))
+lookup' key stat = do
+  sz <- bucketSize <$> readIORef (header stat)
+  let ha = hash key
+  let bix = ha `mod` sz
+  link <- readBucket bix stat
+  findLink emptyEntry link
+
+  where
+    findLink bef cur
+      | cur == emptyEntry =
+        return Nothing
+      | otherwise = do
+        -- TODO: FIXME: when read less than key length
+        (r, whole) <- readPartialRecord cur stat
+        if rkey r == key
+          then do
+          if whole
+            then Just <$> return (r, (bef, cur))
+            else Just . (, (bef, cur)) <$> readCompleteRecord cur r stat
+          else findLink cur (rnext r)
+
+insert :: B.ByteString -> B.ByteString -> HashFileState -> IO ()
+insert key val stat = do
+  sz <- bucketSize <$> readIORef (header stat)
+  let ha = hash key
+  let bix = ha `mod` sz
+  let nr = Record emptyEntry key val
+  toplink <- readBucket bix stat
+  mbv <- lookup' key stat
+  case mbv of
+    Nothing -> do
+      nhead <- addRecord nr stat
+      writeBucket bix nhead stat
+    Just (r, (bef, cur)) -> do
+      let curSize = sizeRecord r
+          newSize = sizeRecord nr
+      -- If current size is larger than new size and
+      -- new size is larger than half of current size,
+      -- then just replace it.
+      if curSize >= newSize && curSize <= newSize * 2
+        then do
+        -- replace
+        writeRecord cur (nr { rnext = toplink }) stat
+        return ()
+        else do
+        -- remove and add
+        -- 1. rewrite before's link
+        when (bef /= emptyEntry) $ do
+          -- if current record has parent
+          writeNext bef (rnext r) stat
+        -- 2. alloc new record
+        nhead <- addRecord (nr { rnext = toplink }) stat
+        -- 3. rewrite bucket's link
+        writeBucket bix nhead stat
+        -- 4. add current to free pool
+        -- TODO
+        return ()
+
+remove :: B.ByteString -> HashFileState -> IO ()
+remove key stat = undefined
+
+--
+
+instance MonadControlIO m => H.DB (HashFile m) where
+  accept key f = withState $ \stat -> do
+    mval <- liftIO $ lookup key stat
+    (act, r) <- f mval
+    case act of
+      H.Replace val -> do
+        liftIO $ insert key val stat
+      H.Remove -> do
+        liftIO $ remove key stat
+      H.Nop -> do
+        return ()
+    return r
+
+withState :: MonadControlIO m => (HashFileState -> HashFile m a) -> HashFile m a
+withState f = do
+  l <- HashFile (asks lock)
+  MC.bracket
+    (liftIO $ takeMVar l)
+    (liftIO . putMVar l)
+    (\_ -> f =<< HashFile ask)
--- Test.hs
+++ Test.hs
@@ -1,12 +1,17 @@
 {-# Language OverloadedStrings #-}
 
 import Database.HongoDB
+import Database.HongoDB.HashFile
 
+import qualified Data.ByteString as B
+import Control.Monad
 import Control.Monad.Trans
 import Data.Enumerator as E
 import Data.Enumerator.List as EL
+import System.Directory
 import System.IO
 
+{-
 main :: IO ()
 main = do
   runHashMem $ do
@@ -22,5 +27,29 @@ main = do
             Just (k, v) -> liftIO (print (k, v)) >> f
             Nothing -> return ()
     run_ $ e $$ f
+  
+  return ()
+-}
+
+fname = "tmp.hgdb"
+
+main :: IO ()
+main = do
+  e <- doesFileExist fname
+  when e $ removeFile fname
+  f <- openHashFile fname
+  runHashFile f $ do
+    set "a" "b"
+    set "c" "d"
+    liftIO . print =<< get "a"
+    liftIO . print =<< get "b"
+    liftIO . print =<< get "c"
+    set "a" "x"
+    liftIO . print =<< get "a"
+    set "a" "xyz"
+    liftIO . print =<< get "a"
+    liftIO . print =<< get "b"
+    liftIO . print =<< get "c"
+  closeHashFile f
   
   return ()
